1ada504290535d0e4356bb5456a122b9543b3019
PejmanAslani <aslanipejman79@gmail.com>
2023-01-08 15:45:48 +0330

    10/18

--- src/componnets/call-routes/global-outbound/GlobalOutboundsForm.tsx
+++ src/componnets/call-routes/global-outbound/GlobalOutboundsForm.tsx
@@ -1,55 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Row } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
+import CheckboxC from '../../reuseables/CheckboxC';
+import TextInputC from '../../reuseables/TextInputC';
 
 const GlobalOutboundsForm = () => {
+  const [state, setState] = useState({
+
+    id: null,
+    name: '',
+    privateRoute: false,
+    sequential: 0,
+    enable: false,
+    description: ''
+  });
   return (
     <Row>
-        <Col md={{ span: 8, offset: 2 }}>
+      <Col md={{ span: 8, offset: 2 }}>
         <h5>Global Outbound Route</h5>
         <hr />
         <Form>
+          <Row>
+            <CheckboxC name="enable" label="Enable" checked={state.enable} setState={setState} />
             <Row>
-              <Col md={6}>
-               <Form.Group>
-                <Form.Label>Global Outbound Routes</Form.Label>
-               <Form.Control
-               required
-               type='text'
-               />
-               </Form.Group>
-              </Col>
-              <Col md={6}>
-               <Form.Group>
-                <Form.Label>Name</Form.Label>
-               <Form.Control
-               required
-               type='text'
-               />
-               </Form.Group>
-              </Col>
+              <CheckboxC name="privateRoute" label="Private Route" checked={state.privateRoute} setState={setState} />
             </Row>
-            <Row>
-            <Col md={6}>
-               <Form.Group>
-                <Form.Label>sequential</Form.Label>
-               <Form.Control
-               required
-               type='text'
-               
-               />
-               </Form.Group>
-              </Col>
-              <Col md={6}>
-               <Form.Group>
-                <Form.Label>Global Outbound Routes</Form.Label>
-               <Form.Control
-               required
-               type='text'
-               />
-               </Form.Group>
-              </Col>
-            </Row>
+          </Row>
+          <Row>
+            <TextInputC
+              name="name"
+              label="Name"
+              value={state.name}
+              require={true}
+              type="text"
+              setState={setState}
+            />
+           
+          </Row>
         </Form>
       </Col>
     </Row>
