6faaff108530a15ae0e46e90480fe89b0dcf2d62
PejmanAslani <aslanipejman79@gmail.com>
2023-01-11 15:15:32 +0330

    10/21

---
+++ src/componnets/reuseables/TextareaCustom.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Col, Form } from 'react-bootstrap'
+
+const TextareaCustom = (props: any) => {
+    const handleChange = (e: any) => {
+        const { name, value } = e.target;
+        props.setState((state: any) => ({
+            ...state,
+            [name]: value,
+        }));
+    };
+    return (
+        <Col md={props.md}>
+            <Form.Group className="mb-3">
+                <Form.Label>{props.label}</Form.Label>
+                <Form.Control
+                    name={props.name}
+                    as={"textarea"}
+                    rows={props.rows}
+                    maxLength={props.lenght}
+                    defaultValue={props.value}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+        </Col>
+    )
+}
+TextareaCustom.defaultProps = {
+    rows: 3,
+    length: 1024,
+    md: 6
+}
+export default TextareaCustom
